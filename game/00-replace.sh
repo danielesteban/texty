f1f3b58ab5f7ad96bc6f913317a68cc10aaedcdc
Daniel Esteban Nombela <dani@gatunes.com>
2025-10-06 17:05:40 +0100

    bugfix

--- game/00-replace.sh
+++ game/00-replace.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for file in /usr/share/nginx/html/*
+for file in /usr/share/nginx/html/assets/*.js /usr/share/nginx/html/editor/assets/*.js
 do
 sed -E \
 "s@__CLIENT__@$CLIENT@g; s@__EDITOR__@$EDITOR@g; s@__SERVER__@$SERVER@g" \
